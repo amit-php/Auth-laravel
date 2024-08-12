e62d4bede4b006a3a8a961f58851a1adb4d18739
amit-php <amit.weavers@gmail.com>
2024-08-12 12:54:14 +0530

    change in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,6 @@
         Welcome to Our Website
     </x-slot>
 
-        <h1 class="text-center">Welcome to Our Website</h1>
+        <h1 class="text-center">Welcome to Our Website test website</h1>
         <p class="text-center">This is a simple Bootstrap webpage with a header, navigation, and footer.</p>
 </x-layout>
